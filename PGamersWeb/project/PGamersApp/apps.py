ce9c41e5edf16e21f61839ae4d8f54a375e6af46
TheFlyDog <nicoalexander993@gmail.com>
2020-11-24 20:08:12 -0300

    Eliminacion de la carpeta Principal para arreglo

--- PGamersWeb/project/PGamersApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PgamersappConfig(AppConfig):
-    name = 'PGamersApp'
--- PGamersWeb/project/pwa/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import manifest, service_worker
-
-# Serve up serviceworker.js and manifest.json at the root
-urlpatterns = [
-    url(r'^serviceworker\.js$', service_worker, name='serviceworker'),
-    url(r'^manifest\.json$', manifest, name='manifest'),
-]
